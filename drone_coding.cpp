f4dd03d6b1b44a0834bd99f90d17a8ec5e7a5b11
ZohaAhsan349 <zahsan.bese25seecs@seecs.edu.pk>
2025-10-24 22:30:10 +0500

    initial commit

--- drone_coding.cpp
+++ drone_coding.cpp
@@ -1,13 +1,35 @@
 //Drone Simulator 
 #include <iostream>
-#include <ctime> //for time variable 
+#include <cstdlib>
+#include <ctime>
 using namespace std;
-
-int main() 
- {  int srand(time(0));
-    int weather = (rand()%2)+1;
-    cout<<weather;
-
-     
-    return 0;
+// prototyping
+void startDay();
+void displaySummary(int&, int&, int&, int&);
+int main() {
+	// initializing variables
+	int battery = 100;
+	int successful = 0;
+	int delayed = 0;
+	int failed = 0;
+	// calling functions
+	startDay();
+	displaySummary(battery, successful, delayed, failed);
+	return 0;
+}
+void startDay()
+{
+	cout << "GOOD MORNING.\n";
+	cout << "Battery = 100%\n";
+	cout << "Your deliveries for today are as follows:\n1. Location A\n2. Location B\n3. Location C\n\n";
+}
+//function to display all results at the end.
+void displaySummary(int& battery, int& successful, int& delayed, int& failed)
+{
+	cout << "FLIGHT SUMMARY:\n";
+	cout << "Battery remaining: " << battery << "%\n";
+	cout << "Total deliveries = 3\n";
+	cout << "Deliveries successful = " << successful << endl;
+	cout << "Deliveries delayed = " << delayed << endl;
+	cout << "Deliveries failed = " << failed << endl;
 }
